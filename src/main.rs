1a585101b65a37b63c799c44290f0a3d62e5a2ce
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-11-25 16:59:50 -0500

    Much simpler lexer almost entirely based on https://github.com/weld-project/weld/blob/master/weld/tokenizer.rs
    * Unlike earlier version, now handles tokens that are not separated by whitespace
    as well, e.g., a=5; Still need to update tests though
    
    * Also start writing out grammar for sculpt

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,31 @@ use regex::Regex;
 extern crate lazy_static;
 
 lazy_static! {
-  static ref KEYWORDS    : Regex = Regex::new(r"^(if|elif|else|and|or|xor|pow|true|false|static|snippet)$").unwrap();
-  static ref IDENTIFIERS : Regex = Regex::new(r"^[A-Za-z$_][A-Za-z0-9$_]*$").unwrap();
+  static ref TOKENS      : Regex = Regex::new(r"[0-9]+|[A-Za-z_][A-Za-z0-9_]*|&&|\|\||!|&|\||\^|>>|<<|==|!=|>=|<=|>|<|\+|-|/|\*|%|\{|\}|\(|\)|\[|\]|=|;|\.|,|\?|:|\S+").unwrap();
+  static ref KEYWORDS    : Regex = Regex::new(r"^(static|snippet)$").unwrap();
+  static ref IDENTIFIERS : Regex = Regex::new(r"^[A-Za-z_][A-Za-z0-9_]*$").unwrap();
   static ref VALUES      : Regex = Regex::new(r"^([0-9]+)$").unwrap();
-  static ref REL_OPS     : Regex = Regex::new(r"^(==|!=|>=|<=|>|<)$").unwrap();
-  static ref ARITH_OPS   : Regex = Regex::new(r"^(\+|-|/|\*|%)$").unwrap();
-  static ref GROUPING_OPS: Regex = Regex::new(r"^(\{|\}|\(|\)|\[|\])$").unwrap();
-  static ref OTHER_OPS   : Regex = Regex::new(r"^(=|;|\.|,)$").unwrap();
 }
 
 fn run_lexer(input_program : &str) {
     // Split string into tokens at whitespaces
     // TODO: Fix this to remove this assumption of tokens being separated by whitespaces.
-    let token_iter = input_program.split_whitespace();
-    for token in token_iter {
+    for cap in TOKENS.captures_iter(input_program) {
+      let ref token = cap[0];
       if KEYWORDS.is_match(token) {
-        print!("Found a keyword");
+        println!("Found a keyword {}", token);
       } else if IDENTIFIERS.is_match(token) {
-        print!("Found an identifier");
+        println!("Found an identifier {}", token);
       } else if VALUES.is_match(token) {
-        print!("Found a value");
-      } else if REL_OPS.is_match(token) {
-        print!("Found a relational operator");
-      } else if ARITH_OPS.is_match(token) {
-        print!("Found an arithmetic operator");
-      } else if GROUPING_OPS.is_match(token) {
-        print!("Found a grouping operator");
-      } else if OTHER_OPS.is_match(token) {
-        print!("Found other operator");
+        println!("Found a value {}", token);
       } else {
-        panic!("Found invalid token {} here!!!", token);
+        println!("Found operator token {}", token);
       }
-      println!(" {}", token);
     }
 }
 
 #[test]
-fn test_lexer() {
+fn test_lexer_with_spaces() {
   let input_program = r"snippet fun ( a , b , c , x , y ) {
                           static x = 0 ;
                           if ( a >= b ) {
@@ -49,6 +37,20 @@ fn test_lexer() {
                             b = y ;
                           } elif ( c >= d ) {
                             m == 5 ;
+                          }
+                        }";
+  run_lexer(input_program);
+}
+
+#[test]
+fn test_lexer_wo_spaces() {
+  let input_program = r"snippet fun(a, b, c, x, y) {
+                          static x = 0;
+                          if (a >= b) {
+                            a = x;
+                            b = y;
+                          } elif (c >= d) {
+                            m == 5;
                           }
                         }";
   run_lexer(input_program);
