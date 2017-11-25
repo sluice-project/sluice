a6d9fb136172e0ebcaa448385823ad43606080c9
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-11-24 19:37:36 -0500

    Some work towards tokenizing, inspired by:
    https://github.com/weld-project/weld/blob/master/weld/tokenizer.rs

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,13 @@ use regex::Regex;
 
 fn main() {
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    let input_string = "foobar is foo or bar or k1 or k2 or k3 or k4";
+    let token_iter = input_string.split_whitespace();
+    for token in token_iter {
+      if Regex::new("^(k1|k2|k3)$").unwrap().is_match(token) {
+        println!("Found a keyword");
+      }
+      println!("{}", token);
+    }
     println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
