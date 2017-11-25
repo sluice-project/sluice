6950e4ab7fbcfe1e26cd3bbc52a052613cb08aa0
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-11-24 23:21:10 -0500

    Elevate keywords into a static variable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 extern crate regex;
-
 use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+  static ref KEYWORDS : Regex = Regex::new("^(k1|k2|k3)$").unwrap();
+}
 
 fn main() {
     let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
     let input_string = "foobar is foo or bar or k1 or k2 or k3 or k4";
     let token_iter = input_string.split_whitespace();
-    let keywords = Regex::new("^(k1|k2|k3)$").unwrap();
     for token in token_iter {
-      if keywords.is_match(token) {
+      if KEYWORDS.is_match(token) {
         println!("Found a keyword");
       }
       println!("{}", token);
