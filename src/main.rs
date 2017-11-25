b458c7cdc30a5f381f43680ea282ed6d3009dc75
Anirudh Sivaraman <sk.anirudh@gmail.com>
2017-11-25 14:26:32 -0500

    Working lexer; assumes tokens are separated by whitespaces
    Will try and fix this later.

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,36 @@ extern crate lazy_static;
 
 lazy_static! {
   static ref KEYWORDS    : Regex = Regex::new(r"^(if|else|and|or|xor|pow|true|false)$").unwrap();
-  static ref VALUES      : Regex = Regex::new(r"^([0-9]+)$").unwrap();
   static ref IDENTIFIERS : Regex = Regex::new(r"^[A-Za-z$_][A-Za-z0-9$_]*$").unwrap();
+  static ref VALUES      : Regex = Regex::new(r"^([0-9]+)$").unwrap();
   static ref REL_OPS     : Regex = Regex::new(r"^(==|!=|>=|<=|>|<)$").unwrap();
-  static ref ARITH_OPS   : Regex = Regex::new(r"^(+|-|/|\*|%)$").unwrap();
-  static ref GROUPING_OPS: Regex = Regex::new(r"^({|}\(|\)|\[|\])$").unwrap();
-  static ref OTHER_OPS   : Regex = Regex::new(r"^(=|;|\.|)$").unwrap();
+  static ref ARITH_OPS   : Regex = Regex::new(r"^(\+|-|/|\*|%)$").unwrap();
+  static ref GROUPING_OPS: Regex = Regex::new(r"^(\{|\}|\(|\)|\[|\])$").unwrap();
+  static ref OTHER_OPS   : Regex = Regex::new(r"^(=|;|\.)$").unwrap();
 }
 
 fn main() {
-    let re = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    let input_string = "foobar is foo or bar or k1 or k2 or k3 or k4";
+    let input_string = "if ( a >= b ) { a = x ; b = y ; } else if ( c >= d ) { m == 5 ; } ";
+    // Split string into tokens at whitespaces
     let token_iter = input_string.split_whitespace();
     for token in token_iter {
       if KEYWORDS.is_match(token) {
-        println!("Found a keyword");
+        print!("Found a keyword");
+      } else if IDENTIFIERS.is_match(token) {
+        print!("Found an identifier");
+      } else if VALUES.is_match(token) {
+        print!("Found a value");
+      } else if REL_OPS.is_match(token) {
+        print!("Found a relational operator");
+      } else if ARITH_OPS.is_match(token) {
+        print!("Found an arithmetic operator");
+      } else if GROUPING_OPS.is_match(token) {
+        print!("Found a grouping operator");
+      } else if OTHER_OPS.is_match(token) {
+        print!("Found other operator");
+      } else {
+        print!("Found nothing here!!!");
       }
-      println!("{}", token);
+      println!(" {}", token);
     }
-    println!("Did our date match? {}", re.is_match("2014-01-01"));
 }
