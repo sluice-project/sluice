5aaf9e9a25d58cfbbdb9c3b8e106bccb20fea403
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-01-16 12:56:23 -0500

    Parser for expressions

--- src/parser.rs
+++ src/parser.rs
@@ -8,6 +8,27 @@ pub enum Operand {
   Values(String),
 }
 
+#[derive(Debug)]
+pub enum BinOpType {
+  Plus,
+  Minus,
+  Mul,
+  Div,
+  Modulo
+}
+
+#[derive(Debug)]
+pub enum ExprRight {
+  BinOp(BinOpType, Operand, Box<ExprRight>),
+  Empty()
+  // TODO: Ignoring conditionals for now
+}
+
+#[derive(Debug)]
+pub enum Expr {
+  Expr(Operand, ExprRight)
+}
+
 pub fn parse_operand(mut token_vector : Vec<Tokens>) -> Result<Operand, &'static str> {
   use tokens::Tokens::*;
   assert_eq!(token_vector.len(), 1, "token_vector too long for parse_operand");
@@ -19,9 +40,36 @@ pub fn parse_operand(mut token_vector : Vec<Tokens>) -> Result<Operand, &'static
   }
 }
 
-// pub fn parse_expr(token_vector : Vec<Tokens>) {
-//   return Expr(parse_operand(token_vector[..1]), parse_expr_right(token_vector[1..]));
-// }
+pub fn parse_expr_right(mut token_vector : Vec<Tokens>) -> Result<ExprRight, &'static str> {
+  use tokens::Tokens::*;
+  if token_vector.len() == 0 {
+    return Ok(ExprRight::Empty());
+  }
+
+  if token_vector.len() < 2 {
+    return Err("invalid input to parse_expr_right"); // TODO: print token_vector as part of err message
+  }
+
+  let op_type = token_vector.remove(0);
+  let operand = token_vector.remove(0);
+  return match op_type {
+    Plus   => Ok(ExprRight::BinOp(BinOpType::Plus,  parse_operand(vec!(operand)).unwrap(), Box::new(parse_expr_right(token_vector).unwrap()) )),
+    Minus  => Ok(ExprRight::BinOp(BinOpType::Minus, parse_operand(vec!(operand)).unwrap(), Box::new(parse_expr_right(token_vector).unwrap()) )),
+    Mul    => Ok(ExprRight::BinOp(BinOpType::Mul,   parse_operand(vec!(operand)).unwrap(), Box::new(parse_expr_right(token_vector).unwrap()) )),
+    Div    => Ok(ExprRight::BinOp(BinOpType::Div,   parse_operand(vec!(operand)).unwrap(), Box::new(parse_expr_right(token_vector).unwrap()) )),
+    Modulo => Ok(ExprRight::BinOp(BinOpType::Modulo,parse_operand(vec!(operand)).unwrap(), Box::new(parse_expr_right(token_vector).unwrap()) )),
+    _      => Err("invalid expr_right") 
+  }
+}
+
+pub fn parse_expr(mut token_vector : Vec<Tokens>) -> Result<Expr, &'static str> {
+  if token_vector.len() == 0 {
+    return Err("insufficient tokens in parse_expr");
+  }
+  let operand_tokens = vec!(token_vector.remove(0));
+  let expr_right_tokens = token_vector;
+  return Ok(Expr::Expr(parse_operand(operand_tokens).unwrap(), parse_expr_right(expr_right_tokens).unwrap()));
+}
 
 pub fn parse_prog(token_vector : Vec<Tokens>)  {
   println!("Within parser, doing nothing");
@@ -31,4 +79,10 @@ pub fn parse_prog(token_vector : Vec<Tokens>)  {
 fn test_parse_operand() {
   let input =  r"5";
   println!("{:?}", parse_operand(super::lexer::get_tokens(input)));
+}
+
+#[test]
+fn test_parse_expr() {
+  let input = r"7%5-5+6";
+  println!("{:?}", parse_expr(super::lexer::get_tokens(input)));
 }
