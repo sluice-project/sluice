e4f458e4b974346edd9658441dbe5d509bd11e71
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-01-24 13:38:14 -0500

    Add one integration test.
    Don't think this is the right way to do it; but it works.

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate lazy_static;
+
+mod token;
+mod lexer_tests;
+mod parser_tests;
+pub mod parser_impl; // TODO: Need to refactor; these should be private
+pub mod tree_fold;
+pub mod lexer;
+pub mod parser;
+pub mod semantic_checker;
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,1 +1,43 @@
 extern crate sculpt;
+
+use sculpt::lexer;
+use sculpt::parser;
+use sculpt::semantic_checker::SymbolTablePass;
+use sculpt::semantic_checker::DefineBeforeUsePass;
+use sculpt::parser_impl::Parsing;
+use sculpt::tree_fold::TreeFold;
+
+use std::collections::HashSet;
+
+#[test]
+#[should_panic(expected="y used before definition")]
+fn test_def_before_use(){
+  let input_program = r"snippet fun(a, b, c, x, y, ) {
+                          static x = 0;
+                          a = x;
+                          b = y;
+                          m = 5;
+                        }
+                        snippet foo(a, b, c, ) {
+                          static x = 1;
+                          x = 5;
+                        }
+                        (foo, fun)
+                        ";
+  // Lexing
+  let tokens = & mut lexer::get_tokens(input_program);
+
+  // parsing
+  let parse_tree = parser::Prog::parse(tokens);
+  assert!(tokens.is_empty(), "Tokens is not empty.");
+  println!("Parse tree: {:?}\n", parse_tree);
+
+  // symbol table generation
+  let mut symbol_table = HashSet::new();
+  SymbolTablePass::visit_prog(&parse_tree, &mut symbol_table);
+  println!("Symbol table: {:?}",symbol_table);
+
+  // Check that identifiers are defined before use
+  let mut definitions = HashSet::new();
+  DefineBeforeUsePass::visit_prog(&parse_tree, &mut definitions);
+}
