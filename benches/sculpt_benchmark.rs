9b762bc1afba474b0b3fd8451dd284551bf8c2fd
Anirudh Sivaraman <sk.anirudh@gmail.com>
2018-01-28 18:37:35 -0500

    Parser benchmark; Must run lexer too for idempotence
    Unfortunately, the parser's run time doesn't increase linearly like the lexer
    Here are the benchmark results
    test bench_lexer        ... bench:     634,854 ns/iter (+/- 105,577)
    test bench_lexer_large  ... bench:   7,007,853 ns/iter (+/- 2,080,387)
    test bench_parser       ... bench:   2,715,119 ns/iter (+/- 562,912)
    test bench_parser_large ... bench: 308,766,296 ns/iter (+/- 57,552,571)
    
    It looks more like quadratic. Must fix this.

--- benches/sculpt_benchmark.rs
+++ benches/sculpt_benchmark.rs
@@ -18,6 +18,38 @@ fn bench_lexer(b: &mut Bencher) {
                           d = 1;
                           x = d;
                         }
+                        ".repeat(100);
+  b.iter(|| { lexer::get_tokens(&input_program); });
+}
+
+#[bench]
+fn bench_parser(b: &mut Bencher) {
+  let input_program = r"snippet foo(a, b, c, ) {
+                          d = 1;
+                          x = d;
+                        }
+                        ".repeat(100);
+  b.iter(|| { let tokens = & mut lexer::get_tokens(&input_program);
+              parser::Prog::parse(tokens); } );
+}
+
+#[bench]
+fn bench_lexer_large(b: &mut Bencher) {
+  let input_program = r"snippet foo(a, b, c, ) {
+                          d = 1;
+                          x = d;
+                        }
                         ".repeat(1000);
   b.iter(|| { lexer::get_tokens(&input_program); } );
+}
+
+#[bench]
+fn bench_parser_large(b: &mut Bencher) {
+  let input_program = r"snippet foo(a, b, c, ) {
+                          d = 1;
+                          x = d;
+                        }
+                        ".repeat(1000);
+  b.iter(|| { let tokens = & mut lexer::get_tokens(&input_program);
+              parser::Prog::parse(tokens); } );
 }
