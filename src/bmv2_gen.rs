eb0b837ddc339960b2c3f757589f7e8c56db8f1c
pravein <praveingk@gmail.com>
2019-01-02 17:42:51 +0800

    handling few assignment operations

--- src/bmv2_gen.rs
+++ src/bmv2_gen.rs
@@ -55,11 +55,15 @@ pub fn handle_persistent_decl<'a> (my_decl :  &VariableDecl<'a>) -> P4Header {
     }
     return my_p4_header;
 }
-pub fn get_p4_header_trans<'a> (node_type : &DagNodeType<'a>) -> P4Header {
+pub fn get_p4_header_trans<'a> (node_type : &'a DagNodeType<'a>) -> P4Header {
     let mut my_p4_header : P4Header = P4Header {meta:String::new(), meta_init:String::new(), register:String::new(), define:String::new()};
+    let mut my_vardecl : VarDecl;
     match &node_type {
         DagNodeType::Decl(my_decl) => {
             // Based on the type, the variable decl should be either a register/meta.
+            // Push it to HashMap
+            //let my_decl_g : VariableDecl<'a> = **my_decl;
+            //decl_map.insert(my_decl.identifier.id_name, my_decl);
             match my_decl.var_type.type_qualifier {
                 TypeQualifier::Transient => {
                     my_p4_header = handle_transient_decl(my_decl);
@@ -69,6 +73,8 @@ pub fn get_p4_header_trans<'a> (node_type : &DagNodeType<'a>) -> P4Header {
                 }
                 _ => {}
             }
+
+
             return my_p4_header;
         }
         _ => {
@@ -76,23 +82,38 @@ pub fn get_p4_header_trans<'a> (node_type : &DagNodeType<'a>) -> P4Header {
         }
     }
 }
+pub fn handle_statement<'a> (my_statement :  &Statement<'a>, node_type : &DagNodeType<'a>,
+    decl_map : &'a HashMap<String, VarDecl> ) -> Vec<String> {
+        let mut my_p4_body = Vec::new();
+
+        match my_statement.lvalue {
+            LValue::Scalar(ref my_id) => {
+                let my_lval : String = String::from(my_id.id_name);
+                let my_decl = decl_map.get(&my_lval).unwrap();
+
+            }
+            _ => {
+                //TODO. Do this for Array
+            }
+        }
+        return my_p4_body;
+    }
 
 
 // Ideally to get both ingress and egress parts of conversion [0] for ingress and [1] for egress and [2] for actions
-pub fn get_p4_body_trans<'a> (node_type : &DagNodeType<'a>) -> Vec<String> {
+pub fn get_p4_body_trans<'a> (node_type : &DagNodeType<'a>, decl_map : &'a HashMap<String, VarDecl>) -> Vec<String> {
     let my_p4_ingress : String = String::new();
     let my_p4_egress : String = String::new();
     let my_p4_commons : String = String::new();
     let mut my_p4_body = Vec::new();
 
     match &node_type {
-        DagNodeType::Cond(my_decl) => {
+        DagNodeType::Cond(my_cond) => {
             // TODO : If Statements
             return my_p4_body;
         }
-        DagNodeType::Stmt(my_decl) => {
-
-            return my_p4_body;
+        DagNodeType::Stmt(my_statement) => {
+            return handle_statement(&my_statement, node_type, decl_map);
         }
         _ => {
             return my_p4_body;
@@ -100,18 +121,40 @@ pub fn get_p4_body_trans<'a> (node_type : &DagNodeType<'a>) -> Vec<String> {
     }
 }
 
-pub fn fill_p4code<'a> (my_dag : &mut Dag<'a>) {
+pub fn fill_p4code<'a> (my_dag :  &mut Dag<'a>) {
+
     for mut my_dag_node in &mut my_dag.dag_vector {
+        let mut decl_map : HashMap<String, VarDecl>= HashMap::new();
         my_dag_node.p4_code.p4_header = get_p4_header_trans(&my_dag_node.node_type);
-        //my_dag_node.p4_code.p4_ingress, my_dag_node.p4_code.p4_egress
-        let my_code : Vec<String> = get_p4_body_trans(&my_dag_node.node_type);
+        // Insert nodes to decl_map
+        match my_dag_node.node_type {
+            DagNodeType::Decl(my_decl) => {
+                let mut my_vardecl : VarDecl;
+                let my_id : String = String::from(my_decl.identifier.id_name);
+                let mut my_varinfo : VarInfo<'a>;
+                match my_decl.var_type.var_info {
+                    VarInfo::BitArray(bit_width, var_size) => {
+                        my_varinfo = VarInfo::BitArray(bit_width, var_size);
+                    }
+                    _ => {
+                        my_varinfo = VarInfo::BitArray(0, 0);
+                    }
+                }
+                let my_typequalifier : TypeQualifier = my_decl.var_type.type_qualifier;
+                my_vardecl = VarDecl{id : my_id, var_info : my_varinfo, type_qualifier : my_typequalifier};
+                decl_map.insert(String::from(my_decl.identifier.id_name), my_vardecl);
+            }
+            _ => {}
+
+        }
+
+        let my_code : Vec<String> = get_p4_body_trans(&my_dag_node.node_type, &decl_map);
 
     }
 }
 
 fn gen_p4_includes<'a> ( p4_file : &mut File) {
     p4_file.write(b"#include <core.p4>\n#include <v1model.p4>\n");
-
 }
 
 fn gen_p4_globals<'a> (my_dag : &Dag<'a>, p4_file : &mut File) {
@@ -161,6 +204,13 @@ fn gen_p4_parser<'a> (my_dag : &Dag<'a>, p4_file : &mut File) {
 
 fn gen_p4_body<'a> (my_dag : &Dag<'a>, p4_file : &mut File) {
     let mut contents : String = String::new();
+
+    for my_dag_node in &my_dag.dag_vector {
+        if (my_dag_node.p4_code.p4_commons.len() != 0) {
+            contents = contents + &my_dag_node.p4_code.p4_commons;
+        }
+    }
+
     contents = contents + &format!("control ingress {{\n");
     for my_dag_node in &my_dag.dag_vector {
         if (my_dag_node.p4_code.p4_ingress.len() != 0) {
