42ecd617c98afa6221b8403bd6bf6e2983f53d8c
vnatesh <vikas.natesh@gmail.com>
2019-03-31 21:48:47 -0400

    update simulation to run sluice on arbitrary topos via run_excersise.py

---
+++ bmv2_sim/README.md
@@ -0,0 +1,3 @@
+This is a bmv2 simulator for sluice programs. The run_demo.sh script takes a sluice program as input, compiles it into P4, and uses the commands and topology file to automatically populate the control plane tables and open a mininet shell
+
+eg. vagrant@p4:~sluice/bmv2_sim$  ./run_demo.sh test1  
--- bmv2_sim/run_demo.sh
+++ bmv2_sim/run_demo.sh
@@ -1,5 +1,3 @@
-sudo mn -c
-
 sluice_file=$1
 sluice_file_path=net-progs/$1.np
 
@@ -9,26 +7,4 @@ cp out/${sluice_file}.p4 bmv2_sim/
 cd bmv2_sim
 source env.sh
 
-P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
-
-SWITCH_PATH=$BMV2_PATH/targets/simple_switch/simple_switch
-
-CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
-
-set -m
-$P4C_BM_SCRIPT ${sluice_file}.p4 --json ${sluice_file}.json
-
-# sudo $SWITCH_PATH >/dev/null 2>&1
-# sudo $SWITCH_PATH test1.json \
-#     -i 0@veth0 -i 1@veth2 -i 2@veth4 -i 3@veth6 -i 4@veth8 \
-#     --nanolog ipc:///tmp/bm-0-log.ipc \
-#     --pcap
-
-sudo python $BMV2_PATH/mininet/1sw_demo.py \
-    --behavioral-exe $BMV2_PATH/targets/simple_switch/simple_switch \
-    --json ${sluice_file}.json
-
-# sleep 2
-# $CLI_PATH test1.json < commands1.txt
-# echo "READY!!!"
-# fg
+make stop; make clean; make run
