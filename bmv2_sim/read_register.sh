950aadf97d0b465d265ddc11549640b5de735c95
vnatesh <vikas.natesh@gmail.com>
2019-04-25 03:31:10 -0400

    way to read whole register array

--- bmv2_sim/read_register.sh
+++ bmv2_sim/read_register.sh
@@ -1,7 +1,12 @@
 sluice_file=$1
+thrift_port=$2
 
 source env.sh
 
 CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
 
-echo "register_read reg1 0" | $CLI_PATH ${sluice_file}.json
+# reads the index 7 in reg1
+# echo "register_read reg1 7" | $CLI_PATH ${sluice_file}.json $thrift_port
+
+# reads the whole array reg1
+echo "register_read reg1" | $CLI_PATH ${sluice_file}.json $thrift_port
