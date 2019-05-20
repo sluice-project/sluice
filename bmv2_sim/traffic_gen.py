e23de36b6476ec0ad7c4788636a69afed27e0406
vnatesh <vikas.natesh@gmail.com>
2019-05-19 22:37:29 -0400

    update simulation to correctly send avg of 2 packets per second

--- bmv2_sim/traffic_gen.py
+++ bmv2_sim/traffic_gen.py
@@ -9,6 +9,7 @@ import re
 import readline
 import numpy as np
 import time
+import string
 
 from scapy.all import sendp, send, srp1, get_if_list, get_if_hwaddr
 from scapy.all import Packet, hexdump
@@ -36,20 +37,29 @@ def main():
         print 'pass 1 arguments: <srcPort>'
         exit(1)
 
-    addrs = [socket.gethostbyname(i) for i in ['10.0.1.1', '10.0.2.2', '10.0.3.3', '10.0.4.4', '10.0.1.5']]
+    # TODO : write ip addrs to a tmp file and read from that file here (put
+    # in same file as thrift_port)
+    this_host = ''.join(re.findall(r'h\d+-', get_if()))[:-1]
+    host_num = ''.join(re.findall(r'\d+', this_host))
+    my_ip = "10.0.%s.%s" % (host_num, host_num)
+    addrs = [socket.gethostbyname(i) for i in ['10.0.1.1', '10.0.2.2', '10.0.3.3'] if i != my_ip]
     srcPort = int(sys.argv[1])    
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('', srcPort))
-    start = time.time()
-    # run simulation for 2 min
-    while time.time() < start + 120:
+
+    while True:
         try:
             # sending packts at a rate of 2 packets per sec, i.e. a mean inter-arrival time
             # of 0.5 sec. 
             data = struct.pack('>I', 0)
+            start = time.time()
+            s.sendto(data, (addrs[0], DPORT))
             time.sleep(np.random.exponential(0.5))
-            for i in addrs:
-                s.sendto(data, (i, DPORT))
+            s.sendto(data, (addrs[1], DPORT))
+            time.sleep(np.random.exponential(0.5))
+
+            # time.sleep(np.random.exponential(1) - (time.time() - start))
 
         except Exception as error:
             print error
